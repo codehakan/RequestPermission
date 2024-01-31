014baf6403a7b65821dddfecbba16a22145aae25
Hakan AKKAYA <code.hakan@gmail.com>
2024-01-31 03:29:21 +0300

    test class and libraries deleted.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,5 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+
 }
